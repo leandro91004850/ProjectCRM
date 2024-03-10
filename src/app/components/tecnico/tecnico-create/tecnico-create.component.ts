8f023a8669fa2d8055c72b9875554b6c948ecf2b
leandroleekrap <leandroleekrap@gmail.com>
2024-03-10 17:03:35 -0300

    Finalizando create-tecnico(enpoint create)

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-
-
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -9,15 +11,58 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./tecnico-create.component.css']
 })
 export class TecnicoCreateComponent implements OnInit {
+  
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+    
+  }
 
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router,
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  
+  create(): void{
+    this.service.create(this.tecnico).subscribe(() => {
+      this.toast.success('Cadastrado realizado com sucesso', 'Cadastro', {timeOut: 7000});
+      this.router.navigate(['tecnicos'])
+    }, exception => {
+      console.log(exception);
+      if(exception.error.errors){
+          exception.error.errors.forEach((element: any) => {
+            this.toast.error(element.message, "Cadastro", {timeOut: 7000});
+          })
+      }else{
+          this.toast.error(exception.error.message , "Cadastro", {timeOut: 7000});
+      }
+    })
+  }
+
+  addPerfil(codigoPerfil: any): void{
+    if(this.tecnico.perfis.includes(codigoPerfil)){
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(codigoPerfil), 1); // remove o perfil
+      console.log(this.tecnico.perfis);
+    }else{
+      this.tecnico.perfis.push(codigoPerfil);
+      console.log(this.tecnico.perfis);
+    }
+
   }
 
   validaCampos(): boolean{
